9d610e4a51829b359043d45562c31e13e9ccb770
reviakin-package <80742342+reviakin-package@users.noreply.github.com>
2021-09-22 19:26:11 +0400

    Update FilmReviewRepository.kt

--- app/src/main/java/com/reviakin_package/test_films_app/domain/repository/FilmReviewRepository.kt
+++ app/src/main/java/com/reviakin_package/test_films_app/domain/repository/FilmReviewRepository.kt
@@ -1,7 +1,10 @@
 package com.reviakin_package.test_films_app.domain.repository
 
+import com.reviakin_package.test_films_app.domain.model.filmreviews.FilmReviews
+import io.reactivex.Single
+
 interface FilmReviewRepository {
 
-    fun getAll()
+    fun getAll() : Single<FilmReviews>
 
 }
